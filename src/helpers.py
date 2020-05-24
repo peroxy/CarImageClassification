74d54f201a86eae11cea2a3c6d049947c660794c
peroxy </>
2020-05-24 11:18:34 +0200

    Initial commit

---
+++ src/helpers.py
@@ -0,0 +1,36 @@
+import sqlite3
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file path
+    :return: Connection object or None
+    """
+    conn = sqlite3.connect(db_file)
+    return conn
+
+
+# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+def plot_images(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+def decode_img(img, width, height):
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    return tf.image.resize(img, [width, height])
+
+
+def preprocess_image(file_path, width, height):
+    img = tf.io.read_file(file_path)
+    img = decode_img(img, width, height)
+    return img
---
+++ src/main.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import helpers
+import config
+
+
+def preprocess_data(rows):
+    data = []
+    counter = 0
+    for row in rows:
+        image = helpers.preprocess_image(row[1], config.IMG_HEIGHT, config.IMG_WIDTH)
+        data.append((image.numpy(), config.bodyShapes[row[0]]))
+        counter += 1
+        print("Pre-processing progress: " + '{0:.2f}'.format(100 * (counter / len(rows))) + "%")
+    return data
+
+
+connection = helpers.create_connection("C:\\dev\\AvtoNetScraper\\src\\backup.db")
+
+with connection:
+    cur = connection.cursor()
+
+    cur.execute(r"SELECT BodyShape, LocalPicturePath FROM Cars WHERE LocalPicturePath like '%\train\%'")
+    trainRows = cur.fetchall()
+    train_data = preprocess_data(trainRows[:1000])
+
+    cur.execute(r"SELECT BodyShape, LocalPicturePath FROM Cars WHERE LocalPicturePath like '%\test\%'")
+    testRows = cur.fetchall()
+    test_data = preprocess_data(testRows[:500])
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+# TODO: save model https://www.tensorflow.org/tutorials/keras/save_and_load#hdf5_format
